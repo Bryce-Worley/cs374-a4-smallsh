207c3681865088a115fd1649fa70b5740aae00ad
Bryce-Worley <worley.bryce@gmail.com>
2025-03-02 09:26:34 -0800

    Cleaned up comments, execution of commands in BG/FG seems to be functioning, signal handler currently uses printf() and may need to be changed to write()

--- worleyb_assignment4.c
+++ worleyb_assignment4.c
@@ -27,6 +27,7 @@ struct command_line{
 	bool is_bg;
 };
 
+
 // parse_input adapted from sample_parse.c provided in assignment4 materials
 struct command_line *parse_input(){
 	char input[INPUT_LENGTH];
@@ -55,10 +56,36 @@ struct command_line *parse_input(){
 }
 
 
+// Signal handler to check on background processes
+void handle_SIGCHLD(int signo){
+	int childStatus;
+	pid_t childPid;
+
+	while((childPid = waitpid(-1, &childStatus, WNOHANG)) > 0){
+		if(WIFEXITED(childStatus)){
+			printf("background pid %d is done: exit value %d\n", childPid, WEXITSTATUS(childStatus));
+			fflush(stdout);
+			printf(": ");
+			fflush(stdout);
+		} else{
+			printf("background pid %d is done: terminated by signal %d\n", childPid, WTERMSIG(childStatus));
+			fflush(stdout);
+			printf(": ");
+			fflush(stdout);
+		}
+	}
+}
+
+
 int main(){
 	struct command_line *curr_command;
 	int childStatus;
-	bool initialized = 0;
+	bool initialized = false;
+
+	struct sigaction SIGCHLD_action = {0};
+	SIGCHLD_action.sa_handler = handle_SIGCHLD;
+	SIGCHLD_action.sa_flags = SA_RESTART;
+	sigaction(SIGCHLD, &SIGCHLD_action, NULL);
 
 	while(true){
 		curr_command = parse_input();
@@ -67,11 +94,11 @@ int main(){
 		if(curr_command->argc == 0) continue;
 		if(!strncmp(curr_command->argv[0], "#", 1)) continue;
 		
+		// Built-in commands
 		// Built-in command: exit
 		if(!strcmp(curr_command->argv[0], "exit") && curr_command->argc == 1){
 			break;
 		}
-
 		// Built-in command: cd
 		else if(!strcmp(curr_command->argv[0], "cd") && curr_command->argc == 1){
 			if(chdir(getenv("HOME")) != 0){	
@@ -80,10 +107,8 @@ int main(){
 				fflush(stdout);
 			} else{	
 				setenv("PWD", getenv("HOME"), 1);
-				//printf("You are now in %s\n", getenv("HOME"));
-				//printf("You are now in %s\n", getenv("PWD"));
-				//fflush(stdout);
 			}
+			continue;
 		} else if(!strcmp(curr_command->argv[0], "cd") && curr_command->argc == 2){
 			if(chdir(curr_command->argv[1]) != 0){	
 				perror("chdir() failed");
@@ -91,12 +116,11 @@ int main(){
 				fflush(stdout);
 			} else{	
 				setenv("PWD", curr_command->argv[1], 1);
-				//printf("You are now in %s\n", getenv("HOME"));
 				printf("You are now in %s\n", getenv("PWD"));
 				fflush(stdout);
 			}
+			continue;
 		}
-
 		// Built-in command: status
 		else if(!strcmp(curr_command->argv[0], "status") && curr_command->argc == 1){
 			if(initialized){
@@ -110,73 +134,82 @@ int main(){
 			} else{
 				return 0;
 			}
+			continue;
 		}
+
 
 		// Execution of other commands
-		else{
-			pid_t spawnPid = fork();
-			initialized = 1;
+		pid_t spawnPid = fork();
+		initialized = true;
 
 
-			switch(spawnPid){
-				case -1:
-					perror("fork()\n");
-					exit(1);
-					break;
-				case 0:
-					//printf("CHILD: (%d) running %s command\n", getpid(), curr_command->argv[0]);
-					// Check for input redirection
-					if(curr_command->input_file){
-						//inputRedirect(curr_command->input_file);
-						// Open source file
-						int sourceFD = open(curr_command->input_file, O_RDONLY);
-						if(sourceFD == -1){
-							printf("cannot open %s for input\n", curr_command->input_file);
-							fflush(stdout);
-						//	perror("curr_command->input_file");
-							exit(1);
-						}
-						// Redirect stdin to source file
-						int result = dup2(sourceFD, 0);
-						if(result == -1){
-							perror("source dup2()");
-							exit(1);
-						}
+		switch(spawnPid){
+			case -1:
+				perror("fork()");
+				exit(1);
+				break;
+			case 0: // Child Process
+			// Check for input redirection
+				if(curr_command->input_file){
+					//inputRedirect(curr_command->input_file);
+					// Open source file
+					int sourceFD = open(curr_command->input_file, O_RDONLY);
+					if(sourceFD == -1){
+						printf("cannot open %s for input\n", curr_command->input_file);
+						fflush(stdout);
+						exit(1);
 					}
-
-					// Check for output redirection
-					if(curr_command->output_file){
-						//outputRedirect(curr_command->output_file);
-						int targetFD = open(curr_command->output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
-						if(targetFD == -1){
-							printf("cannot open %s for output\n", curr_command->output_file);
-							fflush(stdout);
-				//			perror("target open()");
-							exit(1);
-						}
-						// Redirect stdouut to target file
-						int result = dup2(targetFD, 1);
-						if(result == -1){
-							perror("target dup2()");
-							exit(1);
-						}
+					// Redirect stdin to source file
+					int result = dup2(sourceFD, 0);
+					if(result == -1){
+						perror("source dup2()");
+						exit(1);
 					}
-					execvp(curr_command->argv[0], curr_command->argv);
-					perror(curr_command->argv[0]);
-					exit(1);
-					break;
-				default:
-					spawnPid = waitpid(spawnPid, &childStatus, 0);
-					// printf("PARENT (%d): child (%d) terminated\n", getpid(), spawnPid);
-					break;
-			}
-	//		if(curr_command->input_file){
-	//			close(sourceFD);
-	//		}
-	//		if(curr_command->output_file){
-	//			close(targetFD);
-	//		}
+					close(sourceFD);
+				} else if(curr_command->is_bg){
+					//Redirect stdin to /dev/null/ for bg processes
+					int bgDefIn = open("/dev/null", O_RDONLY);
+					dup2(bgDefIn, 0);
+					close(bgDefIn);
+				}
+
+				// Check for output redirection
+				if(curr_command->output_file){
+					//outputRedirect(curr_command->output_file);
+					int targetFD = open(curr_command->output_file, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+					if(targetFD == -1){
+						printf("cannot open %s for output\n", curr_command->output_file);
+						fflush(stdout);
+						exit(1);
+					}
+					// Redirect stdouut to target file
+					int result = dup2(targetFD, 1);
+					if(result == -1){
+						perror("target dup2()");
+						exit(1);
+					}
+					close(targetFD);
+				} else if(curr_command->is_bg){
+					// Redirect stdout to /dev/null for bg process
+					int bgDefOut = open("/dev/null", O_WRONLY);
+					dup2(bgDefOut, 1);
+					close(bgDefOut);
+				}
+
+				execvp(curr_command->argv[0], curr_command->argv);
+				perror(curr_command->argv[0]);
+				exit(1);
+				break;
+			default://Parent process
+				if(curr_command->is_bg){
+					printf("background pid is %d\n", spawnPid);
+					fflush(stdout);
+				} else{
+					waitpid(spawnPid, &childStatus, 0);
+				}
+				break;
 		}
+	
 	}
 	return EXIT_SUCCESS;
 }
